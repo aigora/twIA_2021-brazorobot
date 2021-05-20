af1d773843d82073eb5e77759abe0a6363c40bc6
davidpintollorente <80397735+davidpintollorente@users.noreply.github.com>
2021-05-20 21:51:41 +0200

    Update Comunicacion_Receptor.ino

--- SOFTWARE/Comunicacion_entre_nRF24L01/Comunicacion_Receptor/Comunicacion_Receptor.ino
+++ SOFTWARE/Comunicacion_entre_nRF24L01/Comunicacion_Receptor/Comunicacion_Receptor.ino
@@ -1,8 +1,10 @@
 #include <SPI.h>
 #include <RF24.h>
 
-RF24 radio(7,8); //Conexiones pin CNS, CE
-const byte acceso[] = "10000"; //Pin de acceso
+RF24 radio(7,8); //Conexiones pin CE|CSN
+const uint64_t acceso = 0xE8E8F0F0E1LL; //Codigo de acceso
+
+int dato;
 
 void setup() {
   //Inicia monitor en serie
@@ -10,7 +12,7 @@ void setup() {
   //Inicia la señal
   radio.begin();
   //Misma direccion el el trasmisor
-  radio.openReadingPipe(0, acceso);
+  radio.openReadingPipe(1, acceso);
   //Establece un nivel bajo de potencia
   radio.setPALevel(RF24_PA_MIN);
   //Declarar receptor
@@ -19,12 +21,11 @@ void setup() {
 }
 
 void loop() {
+   //Si recibe algo
   if(radio.available()){
-    //Creamos una cadena donde recibir el mensaje
-    char texto[32] = "";
-    //Recibimos la informacion por Radio Frecuencia
-    radio.read(&texto,sizeof(texto));
-    //Eviamos lo que hemos recibido al monitor
-    Serial.println(texto);
+      radio.read(dato, sizeof dato); 
+      //Imprimelo
+      Serial.println(dato);
   }
+  delay(1000);
 }
