05d188412b31f41b08e1969bc38b00c7a30eb69b
davidpintollorente <80397735+davidpintollorente@users.noreply.github.com>
2021-05-23 22:32:26 +0200

    Update PROGRAMA_MEGA.ino

--- SOFTWARE/PROGRAMA_MEGA/PROGRAMA_MEGA.ino
+++ SOFTWARE/PROGRAMA_MEGA/PROGRAMA_MEGA.ino
@@ -8,8 +8,10 @@ const int MotorApin3 = 3;
 const int MotorBpin2 = 5;
 const int MotorBpin1 = 4;
 
-RF24 radio(7,8); //Conexiones pin CNS, CE
-const byte acceso[] = "10000"; //Pin de acceso
+RF24 radio(7,8); //Conexiones pin CE|CSN
+const uint64_t acceso = 0xE8E8F0F0E1LL; //Codigo de acceso
+
+int dato;
 
 void setup() {
   //Inicia monitor en serie
@@ -33,36 +35,34 @@ void setup() {
 
 void loop() {
   if(radio.available()){
-    //Creamos una cadena donde recibir el mensaje
-    char texto[3] = "";
     //Recibimos la informacion por Radio Frecuencia
-    radio.read(&texto,sizeof(texto));
+    radio.read(dato,sizeof dato);
 
-    if(texto == "01"){
+    if(dato == 01){
       digitalWrite(3, HIGH); 
       digitalWrite(2, LOW);
       Serial.print("La muñeca esta bajando\n");
       delay(1000);
     }
-    if(texto == "10"){
+    if(dato == 10){
       digitalWrite(2, HIGH); 
       digitalWrite(3, LOW);
       Serial.print("La muñeca esta subiendo\n");
       delay(1000);
     }
-    if(texto == "02"){
+    if(dato == 02){
       digitalWrite(4, HIGH); 
       digitalWrite(5, LOW);
       Serial.print("Las pinzas se estan abriendo\n");
       delay(1000);
     }
-    if(texto == "20"){
+    if(dato == 20){
       digitalWrite(5, HIGH); 
       digitalWrite(4, LOW);
       Serial.print("Las pinzas se estan cerrando\n");
       delay(1000);
     }
-    if(texto == "00"){
+    if(dato == 00){
       digitalWrite(2, LOW); 
       digitalWrite(3, LOW);
       digitalWrite(4, LOW); 
